8309f1dedbc64c0d155e288f1c8875e5452e901d
plumberDong <83227411+plumberDong@users.noreply.github.com>
2024-01-29 16:51:33 +0800

    删掉keys
    delete keys

--- utils/123.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-dat_path = 'datasets/shxyj_2023_6.csv'
-# reading the xls file into a pandas dataframe
-df = pd.read_csv(dat_path, encoding = 'gbk')
-print(df)
--- utils/api_keys.py
+++
@@ -1,2 +0,0 @@
-api_key = "sk-IfePkz8qxlALNoHZ55A8A548191a41D8990d8c53Ed9c0344",
-api_base= "https://api.132006.xyz/v1",
